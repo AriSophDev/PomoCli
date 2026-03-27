e5c7c76c33f9e3252344f590340d95c82873a8d3
Iana Sophia <111167233+AriSophDev@users.noreply.github.com>
2026-03-27 10:08:32 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "ui.hpp"
 
 int main() {
-    int work = 1;
-    int rest = 1;
+    int work = 25;
+    int rest = 5;
     int cycles = 4;
 
     // Llamamos a la interfaz que creamos en ui.cpp
